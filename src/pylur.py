ba7e841747119ee65618739b9c6a09c280c6f1ff
Kejia <w.kejia@gmail.com>
2017-06-16 01:05:06 -0400

    adding clear method

--- src/pylur.py
+++ src/pylur.py
@@ -1,18 +1,42 @@
 
 from llist import dllist, dllistnode
 class LRU(object):
+    """
+    this is a class providing the `least recently used' (LRU) cache eviction policy.
+    """
     def __init__(self, capacity):
+        """
+        arg:
+            capacity: a positive integer, indicating the capacity of the cache
+        val:
+            none.
+        """
         self.capacity = capacity
         self.size = 0
         self.__hashmap = {}
         self.__cache = dllist()
     def get(self, key):
+        """
+        get a value by the key: if the cache contains the key, retrieve the value from cache; otherwise, return None.
+        arg:
+            key: the key associated with a value
+        val:
+            the value mapped to the key if it's in cache; otherwise, None.
+        """
         r = None
         if key in self.__hashmap:
             r = self.__cache.remove(self.__hashmap[key])
             self.__cache.appendleft(r)
         return r.value
     def put(self, key, val):
+        """
+        associate the value with the key, and put the entry into cache.
+        arg:
+            key: the key associated with val
+            val: the value associating with key
+        val:
+            None.
+        """
         v = dllistnode(val)
         if key in self.__hashmap:
             self.__cache.remove(self.__hashmap[key])
@@ -22,3 +46,10 @@ class LRU(object):
             self.size += 1
         else: self.__hashmap.remove(self.__cache.pop())
         self.__cache.appendleft(v)
+    def clear(self):
+        """
+        clear the cache, and reset all parameters.
+        """
+        self.__hashmap = {}
+        self.__cache = dllist()
+        self.size = 0
